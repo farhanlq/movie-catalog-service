ffe879379389b019bd6e570ca5273cf3dcd8884c
Farhan Laeeq <far619@gmail.com>
2019-09-23 13:33:08 +0530

    Updated model folder

--- src/main/java/com/example/microservices/moviecatalogservice/controller/MovieCatalogController.java
+++ src/main/java/com/example/microservices/moviecatalogservice/controller/MovieCatalogController.java
@@ -1,21 +1,42 @@
 package com.example.microservices.moviecatalogservice.controller;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.microservices.moviecatalogservice.model.CatalogItem;
+import com.example.microservices.moviecatalogservice.model.Movie;
+import com.example.microservices.moviecatalogservice.model.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		return Collections.singletonList(new CatalogItem("Transformers", "Test", 4));
+
+		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId,
+				UserRating.class);
+
+		return ratings.getUserRating().stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+
+		})
+
+				.collect(Collectors.toList());
+
+		// for each movie id, call movie info service and get details
+
+		// put them all together
 	}
 }
