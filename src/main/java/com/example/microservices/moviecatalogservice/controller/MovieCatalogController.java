dabc8034a9f824d07cfd5b17b27a1b3eea93f775
Farhan Laeeq <far619@gmail.com>
2019-09-23 16:05:53 +0530

    Added Annotation for Eureka Client

--- src/main/java/com/example/microservices/moviecatalogservice/controller/MovieCatalogController.java
+++ src/main/java/com/example/microservices/moviecatalogservice/controller/MovieCatalogController.java
@@ -24,11 +24,11 @@ public class MovieCatalogController {
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId,
+		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId,
 				UserRating.class);
 
 		return ratings.getUserRating().stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 			return new CatalogItem(movie.getName(), "Desc", rating.getRating());
 
 		})
